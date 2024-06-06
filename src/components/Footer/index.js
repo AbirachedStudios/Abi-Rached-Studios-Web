000395b816e90e23877d3d07dc10d22f2e678dd8
kyrke <martin_matias@hotmail.com.ar>
2024-06-06 19:50:10 -0300

    maping footer items added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,35 +1,57 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import logo from "../../assets/img/ARS-VECTOR.png";
 import linkedinIcon from "../../assets/icons/Linkedin Logo.svg";
 import facebookIcon from "../../assets/icons/Facebook Logo.svg";
 import instagramIcon from "../../assets/icons/Instagram Logo.svg";
+import { footerNavItems } from "@/data/footerItems";
 import { paths } from "@/data/paths";
 const Footer = () => {
+  const [lang, setLang] = useState("default");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedLang = localStorage.getItem("lang") || "default";
+      setLang(storedLang);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("lang", lang);
+    }
+    console.log("Language set to:", lang); // Debugging line
+  }, [lang]);
+
+  const handleLanguageChange = (lang) => {
+    setLang(lang);
+  };
   return (
     <footer className="bg-neutral-20 text-neutral-100 p-16 justify-between">
       <div className="flex flex-col lg:flex-row items-start gap-16">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center mt-8 max-w-[350px]">
           <div className="flex flex-col items-center gap-7">
-          <a
-            href={paths.home}
-            className="flex flex-wrap items-center space-x-3 rtl:space-x-reverse"
-          >
-            <Image
-              src={logo}
-              alt="Abi Rached Studios logo"
-              width={75}
-              height={75}
-            />
-            <div>
-              <p className="font-headline-large text-headline-large text-gold font-bold">
-                ABI RACHED
-              </p>
-              <p className="font-headline-large text-headline-large text-gold font-bold">
-                STUDIOS
-              </p>
-            </div>
-          </a>
+            <a
+              href={paths.home}
+              className="flex flex-wrap items-center space-x-3 rtl:space-x-reverse"
+            >
+              <Image
+                src={logo}
+                alt="Abi Rached Studios logo"
+                width={75}
+                height={75}
+              />
+              <div>
+                <p className="font-headline-large text-headline-large text-gold font-bold">
+                  ABI RACHED
+                </p>
+                <p className="font-headline-large text-headline-large text-gold font-bold">
+                  STUDIOS
+                </p>
+              </div>
+            </a>
             <div className="flex justify-center space-x-4">
               <a href="#" className="text-neutral-100 hover:text-primary-60">
                 <Image src={linkedinIcon} alt="LinkedIn" />
@@ -47,30 +69,22 @@ const Footer = () => {
           <div>
             <h3 className="text-gold mb-2 font-title-large-bold">NAVEGACIÓN</h3>
             <ul className="space-y-1">
-              <li>
-                <a href="#" className="hover:text-primary-60 font-title-large">
-                  Juegos
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-primary-60 font-title-large">
-                  Inicio
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-primary-60 font-title-large">
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-primary-60 font-title-large">
-                  Contacto
-                </a>
-              </li>
+              {footerNavItems.map((item) => (
+                <li key={item.default}>
+                  <a
+                    href={item.url}
+                    className="hover:text-primary-60 font-title-large"
+                  >
+                    {lang === "es" ? item.es : item.default}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="mt-4 md:mt-0">
-            <h3 className="text-gold mb-2 font-title-large-bold">INSTITUCIONAL</h3>
+            <h3 className="text-gold mb-2 font-title-large-bold">
+              INSTITUCIONAL
+            </h3>
             <address className="not-italic space-y-1">
               <p className="font-title-large">Av. Alicia Moreau de Justo 350</p>
               <p className="font-title-large">(+54) 11-1234-5678</p>
@@ -85,7 +99,9 @@ const Footer = () => {
             </address>
           </div>
           <div className="mt-4 md:mt-0">
-            <h3 className="text-gold mb-2 font-title-large-bold">PRIVACIDAD Y TERMINOS DE USO</h3>
+            <h3 className="text-gold mb-2 font-title-large-bold">
+              PRIVACIDAD Y TERMINOS DE USO
+            </h3>
             <ul className="space-y-1">
               <li>
                 <a href="#" className="hover:text-primary-60 font-title-large">
---
+++ src/data/footerItems.js
@@ -0,0 +1,8 @@
+import { paths } from "./paths";
+
+export const footerNavItems = [
+  { default: "Games", es: "Juegos", url: "#" },
+  { default: "Home", es: "Inicio", url: "#" },
+  { default: "News", es: "Novedades", url: "#" },
+  { default: "Contact", es: "Contacto", url: "#" },
+];
