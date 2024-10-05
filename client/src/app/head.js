f8e467bfb83f4f9c441b233e3419c1b2415a1b62
kyrke <martin_matias@hotmail.com.ar>
2024-10-05 16:22:18 -0300

    Modified tailwind config; Modified and added sections

---
+++ client/src/app/head.js
@@ -0,0 +1,14 @@
+// app/head.js
+export default function Head() {
+    return (
+      <>
+        <title>Home - Abi Rached Studios</title>
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Abi Rached Studios" />
+        <meta name="publisher" content="Abi Rached Studios" />
+        <meta name="description" content="Welcome to Abi Rached Studios. We are an indie video game development studio from Argentina, created by gamers for gamers. Discover our upcoming releases." />
+        <link rel="canonical" href="https://www.tu-sitio.com/" />
+      </>
+    );
+  }
+  
