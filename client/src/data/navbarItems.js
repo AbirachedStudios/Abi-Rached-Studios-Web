1c4f0c8b35ed0c1443df0c3219051ee7240080c0
AbirachedStudios <157771964+AbirachedStudios@users.noreply.github.com>
2026-03-19 17:39:35 -0300

    Delete client directory

--- client/src/data/navbarItems.js
+++
@@ -1,8 +0,0 @@
-import { paths } from "./paths";
-
-export const navbarItems = [
-  { default: "Home", es: "Inicio", url: `${paths.home}` },
-  { default: "News", es: "Novedades", url: `${paths.news}` },
-  { default: "Games", es: "Juegos", url: "#" },
-  { default: "Support", es: "Soporte", url: "#" },
-];
--- client/src/data/paths.js
+++
@@ -1,5 +0,0 @@
-export const paths = {
-    home: "/",
-    news: "/news",
-    terms: "/terms"
-  };
